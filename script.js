02179f05d2e330fe9ebc6b15fece2a33f49a77dc
Lucia Barboza <100799812+luciabarboza@users.noreply.github.com>
2023-06-10 00:23:27 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
-// || CONSTANTS
 const APIKEY = "69edf5c9109661df5a4d334d7e999158"; //enter your own API key here
 const LANGUAGE = "en-US";
 
-// || OTHER VARIABLES
+
 let page = 1;
 let currentSearch = "";
-
-//DOM OBJECTS
 
 const movieInput = document.querySelector("#search-input");
 const modal = document.querySelector(".modal");
@@ -42,11 +39,6 @@ document.addEventListener("click", function (event) {
     }
 });
 
-
-// || ASYNC FUNCTIONS TO GET DATA FROM API
-
-/*This function takes in no parameters. It fetches the data for movies now playing and calls displayResults
-with the responseData. It has no return value*/
 async function getCurrent() {
     currentSearch = "";
     let apiURL = `https://api.themoviedb.org/3/movie/now_playing?api_key=${APIKEY}&language=${LANGUAGE}&page=${page}&include_adult=false`;
@@ -55,24 +47,21 @@ async function getCurrent() {
     movieHeader.innerHTML = `<h2>Now Playing</h2>`
     displayResults(responseData);
 }
-/*This function takes in a movieId. It fetches the data for the details of a single movie and 
-returns the responseData json*/
+
 async function getDetails(movieId) {
     let apiURL = `https://api.themoviedb.org/3/movie/${movieId}?api_key=${APIKEY}&language=${LANGUAGE}`;
     let response = await fetch(apiURL);
     let responseData = await response.json();
     return responseData;
 }
-/*This function takes in a movieId. It fetches the data for the videos of a single movie and 
-returns the responseData json*/
+
 async function getVideos(movieId) {
     let apiURL = `https://api.themoviedb.org/3/movie/${movieId}/videos?api_key=${APIKEY}&language=${LANGUAGE}`;
     let response = await fetch(apiURL);
     let responseData = await response.json();
     return responseData;
 }
-/*This function takes in a query. It fetches the data for the movies given a search query and 
-calls displayResults and changes the DOM*/
+
 async function getResults(query) {
     let apiURL = `https://api.themoviedb.org/3/search/movie?api_key=${APIKEY}&query=${query}&language=${LANGUAGE}&page=${page}&include_adult=false`;
     let response = await fetch(apiURL);
@@ -82,8 +71,7 @@ async function getResults(query) {
     movieMoreDiv.classList.remove("hidden");
     movieMoreBtn.classList.remove("hidden");  
 }
-/*This function takes in responseData from the API calls and updates the DOM to add movie information for
-each movie in the data array. It has no return value*/
+
 function displayResults(responseData) {
     responseData.results.forEach(element => {
         //handle if no poster path
@@ -100,16 +88,12 @@ function displayResults(responseData) {
 } 
 
 
-// || EVENT HANDLERS
-/*This function closes the modal by changing the styling of the modalto display: none. This function
-is fired when a user presses the x button in the corner of the modal*/
+
 function handleClose(evt) {
     modalContent.innerHTML = ``;
     modal.style.display = "none";
 }
 
-/*This function handles a search query by clearing the movie Area, updating variables, and fetching 
-the search data. It is fired when the user clicks the submit button after entering a keyword*/
 function handleFormSubmit(evt) {
     evt.preventDefault();
     //clear current div
@@ -120,9 +104,7 @@ function handleFormSubmit(evt) {
     getResults(searchTerm);
 
 }
-/*This function increments the page variable and if there is an active search then it gets more search
-results, otherwise it gets more now playing results. This function is fired when the Show More Movies 
-button is clicked*/
+
 function handleMore(evt) {
     evt.preventDefault();
     page++;
@@ -132,16 +114,14 @@ function handleMore(evt) {
         getCurrent();
     }
 }
-/*This function clears the movie area, resets the page number, clears the text input field, and gets the
-current movies*/
+
 function handleCurrent(evt) {
     movieArea.innerHTML = ``;
     page = 1;
     movieInput.value = "";
     getCurrent();
 }
-/*This function gets the details of a movie given a movie id and fetches the trailer of the movie. Then
-the DOM is updated to include the modal HTML with details of the movie and trailer.*/
+
 async function handleModal(evt) {
     let movieId = evt.target.attributes[1].nodeValue;
     //another api call to get more details
@@ -182,10 +162,9 @@ async function handleModal(evt) {
 }
 
 
-// || BACK TO TOP BUTTON
-// When the user scrolls down 500px from the top of the document, show the button
+
 window.onscroll = function() {scrollFunction()};
-/*This function makes the back to top button appear or disappear when scrolling*/
+
 function scrollFunction() {
   if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
     mybutton.style.display = "block";
@@ -194,13 +173,11 @@ function scrollFunction() {
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  document.documentElement.scrollTop = 0; 
 }
 
-/*When the website loads, display the Now Playing movies*/
+
 window.onload = function () {
     getCurrent();
 }
